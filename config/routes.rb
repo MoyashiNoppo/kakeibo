1faca899f0a74564d42b8ebebb3441805c87d4a5
MoyashiNoppo <moyashin18@gmail.com>
2022-06-06 09:38:31 +0900

    記入作成完了画面の作成とcreateのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'reports#index'
   resources :reports, only: :index
-  resources :variablecosts, only: :new
+  resources :variablecosts, only: [:new, :create]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
