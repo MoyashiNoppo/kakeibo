e642f7117c4f3a58821bde15b365225fbb6ea553
MoyashiNoppo <moyashin18@gmail.com>
2022-06-06 09:24:54 +0900

    変動費記入画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'reports#index'
   resources :reports, only: :index
+  resources :variablecosts, only: :new
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
